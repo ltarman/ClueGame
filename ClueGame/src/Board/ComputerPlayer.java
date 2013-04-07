658c106771aa88409670aa27b899f8f05e5ad6f0
Leon <ltarman@mymail.mines.edu>
2013-04-07 16:22:00 -0600

    computer accuse and suggest added

--- ClueGame/src/Board/ComputerPlayer.java
+++ ClueGame/src/Board/ComputerPlayer.java
@@ -12,19 +12,26 @@ import Board.BoardCell;
 public class ComputerPlayer extends Player{
 	
 	private char visited;
-	private boolean willAccuse = false;
+	private boolean willAccuse;
+	private ArrayList<Card> solutionGuess;
 
 	public ComputerPlayer(String playerName, ArrayList<Card> cards, String location, String color) {
 		super(playerName, cards, location, color);
+		willAccuse = false;
+		solutionGuess = new ArrayList<Card>();
 	}
 	
 	@Override
 	public void playerTurn(int randomRollValue){
 		Board board = connectGame.getBoard();
+		if(willAccuse){makeAccusation();}
+		
 		board.startTargets(index, randomRollValue);
 		this.index = pickLocation(board.getTargets(),board);
+		
 		if(board.getCellAt(index).isRoom()){
 			visited = board.getCellAt(index).getInitial();
+			solutionGuess = makeSuggestion();
 		}
 		board.repaint();
 
@@ -32,21 +39,21 @@ public class ComputerPlayer extends Player{
 
 	//the computer will make a suggestion based on the see cards lists
 	public ArrayList<Card> makeSuggestion() {
-		ArrayList<Card> returnedCardList = new ArrayList<Card>();
-		if(!willAccuse){
-			returnedCardList = connectGame.computerSuggestion(this);
-			//add the three cards to the seen card lists
-			for(Card i:returnedCardList){connectGame.addToSeenCards(i);}
-		}else{
-			connectGame.makeAccusation(returnedCardList);
-			//display something + reset
-			willAccuse = false;
-		}
-		Card result = connectGame.testSuggestion(this, returnedCardList.get(0), returnedCardList.get(1), returnedCardList.get(2));
+		ArrayList<Card> guess = new ArrayList<Card>();
+		guess = connectGame.computerSuggestion(this);
+		//add the three cards to the seen card lists
+		for(Card i:guess){connectGame.addToSeenCards(i);}
+		//test for possible accusation
+		Card result = connectGame.testSuggestion(this, guess.get(0), guess.get(1), guess.get(2));
 		if(result==null){willAccuse = true;}
 		
-		
-		return returnedCardList;
+		return guess;
+	}
+	
+	private void makeAccusation(){
+		connectGame.makeAccusation(solutionGuess);
+		//display something + reset,game over
+		willAccuse = false;
 	}
 	
 	public int pickLocation(Set<Integer> possibleTargets, Board board) {

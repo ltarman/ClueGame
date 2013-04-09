373810bdba9cb1c005de130baf3b0dca0137797b
Ryan Whitehill <rwhitehi@mines.edu>
2013-04-09 14:21:03 -0600

    Fixed the problem with card dealing; 2:20

--- ClueGame/src/Board/HumanPlayer.java
+++ ClueGame/src/Board/HumanPlayer.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 public class HumanPlayer extends Player{
 	
+	public HumanPlayer(String playerName, String location, String color) {
+		super(playerName, location, color);
+	}
+	
 	public HumanPlayer(String playerName, ArrayList<Card> cards, String location, String color) {
 		super(playerName, cards, location, color);
 	}
